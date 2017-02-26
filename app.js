565f33a9a7a56d08e53de5ddb732049187a5ba82
Shunpei Kobayashi <skonb@me.com>
2017-02-26 17:58:56 +0900

    Fix bugs

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var sockets = require('signal-master/sockets');
 var config = require('signal-master/config/development.json');
-var port = 9000;
+port = Number(process.env.PORT || config.server.port);
 var app = express();
 var server = app.listen(port);
 sockets(server, config); // config is the same that server.js uses
